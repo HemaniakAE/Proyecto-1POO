449c0da1b0456433ceb7a1cba2c9ee6ebc8774d3
HeldyisAE <heldyis1020@gmail.com>
2023-10-18 19:55:11 -0600

    Add files via upload

---
+++ Código/LogicaDeBingo/Configuracion.java
@@ -0,0 +1,12 @@
+package LogicaDeBingo;
+
+
+/**
+ * Enumeration class Configuracion - Se presentan las opciones de configuracion para los juegos
+ * 
+ * @author (Heldyis Agüero)
+ * @version (16/10/2023)
+ */
+public enum Configuracion {
+    JUGARX, CUATROESQUINAS, CARTONLLENO, JUGARZ
+}
