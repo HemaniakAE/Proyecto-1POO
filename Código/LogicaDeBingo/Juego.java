f0d38b03657cd983228a9ddf0e9b1e0ef5a24aaf
HeldyisAE <heldyis1020@gmail.com>
2023-10-19 22:11:18 -0600

    Add files via upload

--- Código/LogicaDeBingo/Juego.java
+++ Código/LogicaDeBingo/Juego.java
@@ -1,6 +1,7 @@
 package LogicaDeBingo;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Random;
@@ -8,114 +9,148 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.time.ZoneId;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.Session;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Transport;
+import javax.mail.MessagingException;
+
 
 /**
- * Clase juego.
- * Abstracción de la clase juego.
- * 
- * @author (Heldyis Agüero Espinoza) 
+ * Clase juego. Abstracción de la clase juego.
+ *
+ * @author (Heldyis Agüero Espinoza)
  * @version (17/10/20)
  */
 public class Juego {
-    
-    private Jugador jugador;
-    private Carton carton;
-    private Configuracion tipoDeJuego;
-    private double premio;
-    private ArrayList<Integer> listaDeBolas;
-    private ArrayList<Jugador> listaJugadores;
-    private static int totalJugadores;
-    private LocalDate fechaDelJuego;
-    private LocalTime horaDelJuego;
-    
-    public Juego(Configuracion pTipoDeJuego, double pPremio) {
-        this.tipoDeJuego = pTipoDeJuego;
-        this.premio = pPremio;
-        this.fechaDelJuego = LocalDate.now(ZoneId.systemDefault()); // Obtiene la fecha actual del sistema
-        this.horaDelJuego = LocalTime.now(ZoneId.systemDefault()); // Obtiene la hora actual del sistema
-        totalJugadores = 0;
-    }
-    
-    public boolean validarJugador(Jugador pJugador) {
-        boolean jugadorRepetido = false; //Variables booleana para control
-        
-        for (Jugador jugadorExistente : listaJugadores) {
-            //Comparación de las cédulas
-            if (jugadorExistente.getCedula() == pJugador.getCedula()) {
-                jugadorRepetido = true;
-                return jugadorRepetido;
-            }
-        }
+
+  private Jugador jugador;
+  private Carton carton;
+  private CuentaCorreo correo;
+  private Configuracion tipoDeJuego;
+  private double premio;
+  public ArrayList<Integer> listaDeBolas;
+  public ArrayList<Jugador> listaJugadores;
+  public static int totalJugadores;
+  private LocalDate fechaDelJuego;
+  private LocalTime horaDelJuego;
+
+  public Juego(Configuracion pTipoDeJuego, double pPremio) {
+    this.tipoDeJuego = pTipoDeJuego;
+    this.premio = pPremio;
+    this.fechaDelJuego = LocalDate.now(ZoneId.systemDefault()); // Obtiene la fecha actual del sistema
+    this.horaDelJuego = LocalTime.now(ZoneId.systemDefault()); // Obtiene la hora actual del sistema
+    totalJugadores = 0;
+    listaJugadores = new ArrayList<>(); // Inicializa la lista de jugadores
+  }
+
+  public boolean validarJugador(Jugador pJugador) {
+    boolean jugadorRepetido = false; //Variables booleana para control
+
+    for (Jugador jugadorExistente : listaJugadores) {
+      //Comparación de las cédulas
+      if (jugadorExistente.getCedula() == pJugador.getCedula()) {
+        jugadorRepetido = true;
         return jugadorRepetido;
+      }
     }
-    
-    public void registrarJugador(String nombreCompleto, int cedula, String correo) {
-        Jugador newPlayer = new Jugador(nombreCompleto, cedula, correo);
-        
-        if (validarJugador(newPlayer)) {
-            //Añade al jugador al juego
-            listaJugadores.add(newPlayer);
-            totalJugadores++;
+    return jugadorRepetido;
+  }
+
+  public void registrarJugador(String nombreCompleto, int cedula, String correo) {
+    Jugador newPlayer = new Jugador(nombreCompleto, cedula, correo);
+
+    if (!validarJugador(newPlayer)) {
+      //Añade al jugador al juego
+      listaJugadores.add(newPlayer);
+      totalJugadores++;
+    }
+  }
+
+  public boolean enviarCartonAJugador(int pCantidadDeCartones, int pCedula) {
+    int totalCartones = Carton.getContadorCartones();
+    if (pCantidadDeCartones <= totalCartones) {
+      Jugador jugadorEncontrado = null;
+      for (Jugador jugador : listaJugadores) {
+        if (jugador.getCedula() == pCedula) {
+          jugadorEncontrado = jugador;
+          break; // Terminar el bucle si se encuentra el jugador
+        }
+      }
+      if (jugadorEncontrado == null) {
+        return false;
+      }
+      List<File> cartonesAleatorios = obtenerArchivosAleatoriosEnCarpeta("Cartones", pCantidadDeCartones);
+
+      if (cartonesAleatorios.isEmpty()) {
+        return false; // No se encontraron archivos
+      }
+      List<String> rutaImagenes = new ArrayList<>(); //Crea una lista que contiene la ruta de las imagenes
+      for (File archivo : cartonesAleatorios) {
+        // Aquí puedes trabajar con el archivo, por ejemplo, cargar o manipular el archivo.
+        String codeCarton = archivo.getName(); //Obtiene el nombre de la imagen, que corresponde a su código
+        jugadorEncontrado.asignarCarton(codeCarton); //Asigna el cartón al jugador correspondiente a la cédula
+        String rutaImagen = archivo.getAbsolutePath(); //Obtiene la ruta absoluta de la imagen
+        rutaImagenes.add(rutaImagen); //Agrega la ruta a la lista
+      }
+      String correoJugador = jugadorEncontrado.getCorreo();
+      String cuerpoCorreo = jugadorEncontrado.toString();
+      
+      CuentaCorreo correoBingo = new CuentaCorreo("bingoteclimon@gmail.com"); //Crea una instancia de correo
+      correoBingo.enviarCorreo(correoJugador, "Cartones de Bingo",cuerpoCorreo , rutaImagenes);
+      return true;
+    }
+    return false;
+  }
+  
+  public List<File> obtenerArchivosAleatoriosEnCarpeta(String nombreCarpeta, int cantidadArchivos) {
+    File carpeta = new File(nombreCarpeta);
+
+    if (!carpeta.exists() || !carpeta.isDirectory()) {
+        return new ArrayList<>(); // La carpeta no existe o no es una carpeta válida
+    }
+
+    File[] archivos = carpeta.listFiles();
+    List<File> archivosAleatorios = new ArrayList<>();
+
+    if (archivos != null) {
+        int totalArchivos = archivos.length;
+        Random random = new Random();
+
+        while (archivosAleatorios.size() < cantidadArchivos && archivosAleatorios.size() < totalArchivos) {
+            int indiceAleatorio = random.nextInt(totalArchivos);
+            File archivoSeleccionado = archivos[indiceAleatorio];
+
+            if (!archivosAleatorios.contains(archivoSeleccionado)) {
+                archivosAleatorios.add(archivoSeleccionado);
+            }
         }
     }
+
+    return archivosAleatorios;
+}
+  public static void main(String[] args) {
+    Configuracion tipoJuego = Configuracion.X; // Reemplaza TU_ENUM_AQUI con el valor de Configuracion que desees
     
-    public boolean enviarCartonAJugador(int pCantidadDeCartones, int pCedula) {
-        int totalCartones = Carton.getContadorCartones();
-        if(pCantidadDeCartones <= totalCartones) {
-            Jugador jugadorEncontrado = null;
-            for (Jugador jugador : listaJugadores) {
-                if (jugador.getCedula() == pCedula) {
-                    jugadorEncontrado = jugador;
-                    break; // Terminar el bucle si se encuentra el jugador
-                }
-            }
-            if(jugadorEncontrado == null) {
-                return false;
-            }
-            // Seleccionar n cartones aleatorios
-            ArrayList<Carton> cartonesSeleccionados = new ArrayList<>();
-            Random rand = new Random();
-            
-            while (cartonesSeleccionados.size() < pCantidadDeCartones) {
-                int indiceAleatorio = rand.nextInt(totalCartones);
-                Carton cartonAleatorio = Carton.listaCartonesGenerados.get(indiceAleatorio);
-                
-                if (!cartonesSeleccionados.contains(cartonAleatorio)) {
-                    cartonesSeleccionados.add(cartonAleatorio);
-                }
-            }
-            // Carpeta para almacenar los cartones seleccionados
-            File carpetaCartonesSeleccionados = new File("cartonesSeleccionados");
-            if (!carpetaCartonesSeleccionados.exists()) {
-                carpetaCartonesSeleccionados.mkdir(); // Crea la carpeta si no existe
-            }
+    double premio = 100.0; // Premio de ejemplo
 
-            // Copiar los archivos de los cartones seleccionados en la carpeta
-            for (Carton carton : cartonesSeleccionados) {
-                String codigoCarton = carton.codigoCarton;
-                File archivoOrigen = new File("cartones/" + codigoCarton + ".png");
-                File archivoDestino = new File("cartonesSeleccionados/" + codigoCarton + ".png");
+    Juego juego = new Juego(tipoJuego, premio);
 
-                try {
-                    Files.copy(archivoOrigen.toPath(), archivoDestino.toPath());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+    // Crea una lista de jugadores de ejemplo
+    juego.listaJugadores = new ArrayList<>();
+    juego.registrarJugador("Heldyis Agüero Espinoza", 123456, "heldyis1020@gmail.com");
 
-            // Enviar los archivos de cartones al correo del jugador
-            String correo = jugadorEncontrado.getCorreo();
-            // Implementa el envío de correo con los archivos adjuntos aquí
+    int cedulaJugador = 123456; // Cédula de un jugador existente
+    int cantidadCartones = 5; // Cantidad de cartones para enviar
 
-            return true;
-            
-            
-        } 
-        return false; 
+    if (juego.enviarCartonAJugador(cantidadCartones, cedulaJugador)) {
+        System.out.println("Se enviaron los cartones al jugador con cédula " + cedulaJugador);
+    } else {
+        System.out.println("No se pudo enviar los cartones al jugador con cédula " + cedulaJugador);
     }
-    
-   
-    
-    
-    
+  }
+  
 }
--- Código/LogicaDeBingo/Jugador.java
+++ Código/LogicaDeBingo/Jugador.java
@@ -1,40 +1,64 @@
 package LogicaDeBingo;
+
 import java.util.ArrayList;
 
 /**
- * Clase Jugador
- * Abstracción en código de los jugadores del bingo
- * 
- * @author (Heldyis Agüero) 
+ * Clase Jugador Abstracción en código de los jugadores del bingo
+ *
+ * @author (Heldyis Agüero)
  * @version (16/10/2023 )
  */
 public class Jugador {
-    
-    private Carton carton;
-    private String nombreCompleto;
-    private int cedula;
-    private String correo;
-    private ArrayList<Carton> cartonesAsignados;
-    
-    public Jugador(String pNombreCompleto, int pCedula,String pCorreo) {
-        this.nombreCompleto = pNombreCompleto;
-        this.cedula = pCedula;
-        this.correo = pCorreo;
+
+  private Carton carton;
+  private String nombreCompleto;
+  private int cedula;
+  private String correo;
+  private ArrayList<String> cartonesAsignados;
+
+  public Jugador(String pNombreCompleto, int pCedula, String pCorreo) {
+    this.nombreCompleto = pNombreCompleto;
+    this.cedula = pCedula;
+    this.correo = pCorreo;
+    cartonesAsignados = new ArrayList<>();
+  }
+
+  public String getNombreCompleto() {
+    return nombreCompleto;
+  }
+
+  public int getCedula() {
+    return cedula;
+  }
+
+  public String getCorreo() {
+    return correo;
+  }
+
+  public void asignarCarton(String pCodeCarton) {
+    cartonesAsignados.add(pCodeCarton);
+  }
+  
+  public ArrayList<String> getCartonesAsignados() {
+    return cartonesAsignados;
+  }
+  
+  public String toString() {
+    StringBuilder cuerpoCorreo = new StringBuilder();
+    cuerpoCorreo.append("Estimado ").append(nombreCompleto).append(",\n\n");
+    cuerpoCorreo.append("Te enviamos a continuación adjuntamos tus cartones para el juego de bingo:\n\n");
+
+    // Agrega los códigos de los cartones asignados
+    cuerpoCorreo.append("Tus cartones:\n");
+    for (String codigoCarton : cartonesAsignados) {
+        cuerpoCorreo.append("- ").append(codigoCarton).append("\n");
     }
-    
-    public String getNombreCompleto() {
-        return nombreCompleto;
-    }
-    
-    public int getCedula() {
-        return cedula;
-    }
-    
-    public String getCorreo() {
-        return correo;
-    }
-    
-    private void asignarCarton(Carton pCarton) {
-        cartonesAsignados.add(pCarton);
-    }
+
+    cuerpoCorreo.append("\n");
+    cuerpoCorreo.append("¡Buena suerte en el juego de bingo!\n\n");
+    cuerpoCorreo.append("Atentamente,\n");
+    cuerpoCorreo.append("El equipo de Bingo");
+
+    return cuerpoCorreo.toString();
+  }
 }
